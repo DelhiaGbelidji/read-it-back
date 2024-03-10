307d296d62d7543d1ddabceb762abc886a8e9050
Delhia0 <delhia.gbelidji@teamoty.io>
2024-03-10 16:21:20 +0100

    add services create, show, index, delete for manuscripts resource

--- src/manuscripts/dto/create-manuscript.dto.ts
+++ src/manuscripts/dto/create-manuscript.dto.ts
@@ -1,1 +1,12 @@
-export class CreateManuscriptDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateManuscriptDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  file_url: string;
+
+  @IsNumber()
+  projectId: number;
+}
--- src/manuscripts/manuscripts.service.ts
+++ src/manuscripts/manuscripts.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateManuscriptDto } from './dto/create-manuscript.dto';
-import { UpdateManuscriptDto } from './dto/update-manuscript.dto';
+// import { UpdateManuscriptDto } from './dto/update-manuscript.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ManuscriptsService {
-  create(createManuscriptDto: CreateManuscriptDto) {
-    return 'This action adds a new manuscript';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createManuscriptDto: CreateManuscriptDto, userId: number) {
+    const userExists = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!userExists) {
+      throw new NotFoundException(`User with ID ${userId} not found.`);
+    }
+
+    const projectExists = await this.prisma.project.findUnique({
+      where: { id: createManuscriptDto.projectId },
+    });
+    if (!projectExists) {
+      throw new NotFoundException(
+        `Project with ID ${createManuscriptDto.projectId} not found.`,
+      );
+    }
+    const manuscript = await this.prisma.manuscript.create({
+      data: {
+        ...createManuscriptDto,
+        userId,
+      },
+    });
+    return manuscript;
   }
 
-  findAll() {
-    return `This action returns all manuscripts`;
+  async findAll() {
+    return await this.prisma.manuscript.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} manuscript`;
+  async findOne(id: number) {
+    return await this.prisma.manuscript.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  update(id: number, updateManuscriptDto: UpdateManuscriptDto) {
-    return `This action updates a #${id} manuscript`;
-  }
+  // async update(id: number, updateManuscriptDto: UpdateManuscriptDto) {
+  //   return `This action updates a #${id} manuscript`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} manuscript`;
+  async remove(id: number) {
+    const manuscript = await this.findOne(id);
+    if (!manuscript) throw new NotFoundException('Manuscript not found');
+
+    await this.prisma.manuscript.delete({
+      where: {
+        id: id,
+      },
+    });
+    return { message: 'Manuscript successfully deleted' };
   }
 }
